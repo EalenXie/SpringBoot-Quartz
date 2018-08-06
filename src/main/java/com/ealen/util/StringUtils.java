b031dc4d75ae2f60e51e2a11497a9718ad5da739
EalenXie <ealenxie@qq.com>
2018-08-06 11:36:12 +0800

    更新

--- src/main/java/com/ealen/util/StringUtils.java
+++ src/main/java/com/ealen/util/StringUtils.java
@@ -9,33 +9,29 @@ import java.util.Map;
  */
 public enum StringUtils {
     getStringUtil;
-
     //是否为空
     public boolean isEmpty(String str) {
-        return (str == null) || (str.length() == 0) || (str.equals(""));
+        return str == null || str.length() == 0;
     }
-
     //去空格
     public String trim(String str) {
         return str == null ? null : str.trim();
     }
-
     //获取Map参数值
     public String getMapString(Map<String, String> map) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (Map.Entry entry : map.entrySet()) {
-            result += entry.getValue() + " ";
+            result.append(entry.getValue()).append(" ");
         }
-        return result;
+        return result.toString();
     }
-
     //获取List参数值
     public String getListString(List<String> list) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
         for (String s : list) {
-            result += s + " ";
+            result.append(s).append(" ");
         }
-        return result;
+        return result.toString();
     }
 
 }
